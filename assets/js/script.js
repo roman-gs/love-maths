3d2f0038485586c8125d11ad44116184b61e18a8
roman-gs <roman_gs@hotmail.fr>
2022-10-03 17:17:49 +0000

    Add multiply game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,9 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+
     } else {
         alert(`Unknown Game Type: ${gameType}`)
         throw `Unknown Game Type: ${gameType}, Aborting!`
@@ -65,6 +68,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented Operator ${operator}`)
         throw `Unimplemented Operator ${operator}, Aborting!`;
@@ -96,9 +101,11 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {
-
+   
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
