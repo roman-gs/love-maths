e40f61746cea5d9ce0d180961dcc79fe3bcf4599
roman-gs <roman_gs@hotmail.fr>
2022-10-03 17:27:14 +0000

    Add substract game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,8 +27,12 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+        
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
 
     } else {
         alert(`Unknown Game Type: ${gameType}`)
@@ -70,6 +74,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented Operator ${operator}`)
         throw `Unimplemented Operator ${operator}, Aborting!`;
@@ -100,8 +106,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {
-   
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
