0cc9723f51b8d1e073f1ca0f6f65c4bfcc2f63a2
roman-gs <roman_gs@hotmail.fr>
2022-10-03 17:44:46 +0000

    Add favicon and tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,21 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame("addition");
 })
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed */
 function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
+    // Create two randoms number between 1 & 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
